26805beda883755be0ede6703f1d8f57430214b4
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-12-20 14:31:39 +0100

    Added doc/LinkDef.h.

---
+++ doc/LinkDef.h
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright 2016 -- 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+/*****************************************************************************
+ * @file       LinkDef.h
+ * @brief      A header file defining a logical hierarchy of groups/subroups
+ *             for the documentation of the current project.
+ * @author     did, fab
+ * @details    The structure is as follows:
+ *             [+] cFp_HelloThemisto
+ *
+ * @note: Every cFp_<Project> must be part of the 'cFp' module.
+ *****************************************************************************/
+
+
+/*****************************************************************************
+ *
+ *  cFp_HelloThemisto : This is a 'Hello World' project targetting the SHELL of 
+ *   type 'Themisto' which is a shell with advanced support for accessing the
+ *   hardware components of the FPGA card (compared to type 'Kale').
+ *
+ *****************************************************************************/
+
+/** \defgroup cFp_HelloThemisto cFp_HelloThemisto
+ *  @ingroup cFp
+ * 
+ *  \brief This is the root of the current 'cFp_HelloThemisto' project.
+ */
+
+/*****************************************************************************
+ *
+ *  cFp_HelloThemisto / Submodules
+ *
+ *****************************************************************************/
